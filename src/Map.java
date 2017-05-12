b0ce86d6dc118566ae3ad0ca64d48e77b31c8017
Pavel <nedosika@mail.ru>
2017-05-12 11:24:50 +0300

    map

---
+++ src/Map.java
@@ -0,0 +1,26 @@
+
+/**
+ * Created by Павел on 12.05.2017.
+ */
+public class Map {
+    public String[][] map;
+
+    public Map(int x, int y){
+        map = new String[x][y];
+        for (int i = 0; i < x; i++){
+            for (int j = 0; j < y; j++) {
+                map[i][j] = "#";
+            }
+        }
+    }
+
+    public void print(){
+        for (int i = 0; i < map.length; i++){
+            for (int j = 0; j < map[i].length; j++) {
+                System.out.print(map[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+}
