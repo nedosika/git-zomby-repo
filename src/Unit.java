ba71f57cd8cdae085ce5e792625f1f609647f1fc
Pavel <nedosika@mail.ru>
2017-05-12 12:52:02 +0300

    create unit

---
+++ src/Unit.java
@@ -0,0 +1,23 @@
+import com.sun.javafx.geom.Vec2d;
+
+/**
+ * Created by Павел on 12.05.2017.
+ */
+public class Unit {
+    public String name;
+    public int hp;
+    public int level;
+    public int exp;
+    public Vec2d position;
+    public String type;
+
+    public Unit(String name, int hp, Vec2d position, String type){
+        this.name = name;
+        this.hp = hp;
+        this.level = 1;
+        this.exp = 0;
+        this.position = position;
+        this.type = type;
+    }
+
+}
