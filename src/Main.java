c7fdd94c9f3b657e3ae917b840308d222cf35899
Pavel <nedosika@mail.ru>
2017-05-12 09:02:51 +0300

    создание объекта класса Game

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("NewHelloWorld");
+        //System.out.println("NewHelloWorld");
+        Game game = new Game();
+        game.hello();
     }
 }
