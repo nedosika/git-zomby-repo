165e97e55fffb027addc5aef87071c2a06dd782c
nedosika <nedosika@mail.ru>
2017-05-14 11:56:52 +0300

    create class Emprty and Wall

---
+++ src/Empty.java
@@ -0,0 +1,10 @@
+/**
+ * Created by nedos on 14.05.2017.
+ */
+public class Empty extends Unit {
+    public Empty() {
+
+        //super("empty", 0, "Empty", (char)27 + "[32m" + "." + (char)27 + "[0m", 0);
+        super("empty", 0, "Empty", ".", 0);
+    }
+}
---
+++ src/Wall.java
@@ -0,0 +1,9 @@
+/**
+ * Created by nedos on 14.05.2017.
+ */
+public class Wall extends Unit {
+
+    public Wall() {
+        super("wall", 0, "wall", "#", 0);
+    }
+}
